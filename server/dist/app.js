f7d491b04df296bbaa335009c18665d8447aa52a
Helsh1995 <shawnhellinckx@gmail.com>
2018-05-15 23:15:28 +0200

    added angular cli

--- server/dist/app.js
+++ server/dist/app.js
@@ -10,6 +10,12 @@ class App {
     init(port = 3000) {
         this.app.use(bodyParser.urlencoded({ extended: false }));
         this.app.use(bodyParser.json());
+        this.app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            res.header('Access-Control-Allow-Methods', '*');
+            next();
+        });
         this.app.listen(port, (err) => {
             if (err) {
                 console.log(err);
--- server/dist/routes/mail.route.js
+++ server/dist/routes/mail.route.js
@@ -2,6 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const nodemailer = require("nodemailer");
+const mail_config_1 = require("../config/mail.config");
+const send_mail_validator_1 = require("../validators/send_mail.validator");
+const check_1 = require("express-validator/check");
 class MailRoute {
     constructor() {
         this._router = express_1.Router();
@@ -10,37 +13,35 @@ class MailRoute {
         this._router.get('/', ((req, res, next) => {
             res.send('You really want to send an email?');
         }));
-        this._router.post('/', this.sendMail);
+        this._router.post('/', send_mail_validator_1.SEND_MAIL_VALIDATOR, this.sendMail);
         return this._router;
     }
     sendMail(req, res, next) {
-        console.log(req.body);
+        const errors = check_1.validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send({ errors: errors.array() });
+        }
         const transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: 465,
-            secure: false,
+            secure: true,
             auth: {
-                user: 'shawnhellinckx@gmail.com',
-                pass: 'InesjeLove1003'
-            },
-            proxy: 'http://clientproxy.basf.net:8080/'
+                user: mail_config_1.MAIL_CONFIG.mail,
+                pass: mail_config_1.MAIL_CONFIG.pwd
+            }
         });
         const mailOptions = {
-            from: 'shawnhellinckx@gmail.com',
+            from: req.body.from,
             to: req.body.to,
             subject: req.body.subject,
-            text: req.body.message
+            html: req.body.message
         };
         transporter.sendMail(mailOptions, (err, info) => {
             if (err)
-                res.status(400).send(`The mail couldn't be sended`);
+                res.status(400).send(`The mail couldn't be send`);
             if (info)
-                res.status(200).send(`${info}`);
-            console.log(err);
-            console.log(info);
+                res.status(200).send(`${JSON.stringify(info)}`);
         });
-        // res.status(200)
-        //     .send('The email was sent my friend');
     }
 }
 exports.MailRoute = MailRoute;
